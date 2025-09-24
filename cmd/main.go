0b6552ba491f78650d629e4e307767689a79a992
d.sopp <d.sopp@550550.ru>
2025-09-24 17:07:24 +0800

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"go-links-shorter/internal/hello"
+	"net/http"
+)
+
+func main() {
+	//conf := configs.LoadConfig()
+	router := http.NewServeMux()
+	hello.NewHelloHandler(router)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
+}
